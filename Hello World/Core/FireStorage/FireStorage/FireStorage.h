29ae1e2ef0f0eef9a428ad0fbe66c01f9cb2f033
Jessica Serqueira <jessicaserqueira@MacBook-Pro-de-Glaycon.local>
2023-05-17 16:41:33 -0300

    feat: crianção do projeto

---
+++ Hello World/Core/FireStorage/FireStorage/FireStorage.h
@@ -0,0 +1,18 @@
+//
+//  FireStorage.h
+//  FireStorage
+//
+//  Created by Jessica Serqueira on 17/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FireStorage.
+FOUNDATION_EXPORT double FireStorageVersionNumber;
+
+//! Project version string for FireStorage.
+FOUNDATION_EXPORT const unsigned char FireStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireStorage/PublicHeader.h>
+
+
---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Jessica Serqueira on 17/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
